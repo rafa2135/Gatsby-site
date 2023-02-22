d11903f6be4b8e7e702448ff71234ac48d5a5563
Rafael Villalobos <rafaelvillalobos_20@hotmail.com>
2023-02-22 17:36:41 -0400

    added contact recipes and tags to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,6 +13,15 @@ const Navbar = (props: Props) => {
         <li>
           <Link to="/about">About</Link>
         </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link to="/recipes">Recipes</Link>
+        </li>
+        <li>
+          <Link to="/tags">Tags</Link>
+        </li>
       </ul>
     </nav>
   );
