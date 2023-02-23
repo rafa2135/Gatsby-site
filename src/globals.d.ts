87375ed660684080c4cd1ce9258c132463c7968f
Rafael Villalobos <rafaelvillalobos_20@hotmail.com>
2023-02-23 03:14:52 -0400

    testing gatsby image plugin

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -6,3 +6,7 @@ declare module "*.svg" {
   const value: any;
   export = value;
 }
+declare module "*.jpg" {
+  const value: any;
+  export = value;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import { PageProps, Link } from "gatsby";
 import Layout from "../components/Layout";
+import Images from "../examples/images";
 
 const Index = ({}: PageProps<{}>) => (
   <Layout>
     <div>
       <h1>home page</h1>
+      <Images></Images>
     </div>
   </Layout>
 );
