8535dd22fb29cd1e8c7537f43fea991660d8d3a9
Rafael Villalobos <rafaelvillalobos_20@hotmail.com>
2023-02-23 23:30:59 -0400

    added content to home and about pages

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,34 @@
 import * as React from "react";
 import { PageProps } from "gatsby";
 import Layout from "../components/Layout";
-
+import { StaticImage } from "gatsby-plugin-image";
+import { Link } from "gatsby";
 const About = ({}: PageProps) => (
   <Layout>
-    <h1>About page</h1>
+    <main className="page">
+      <section className="about-page">
+        <article>
+          <h2></h2>
+          <p>
+            Taxidermy forage glossier letterpress heirloom before they sold out
+            you probably haven't heard of them banh mi biodiesel chia.
+          </p>
+          <p>
+            Taiyaki tumblr flexitarian jean shorts brunch, aesthetic salvia
+            retro.
+          </p>
+          <Link to="/contact" className="btn">
+            Contact
+          </Link>
+        </article>
+        <StaticImage
+          src="../assets/images/about.jpeg"
+          alt="pouring salt"
+          className="about-img"
+          placeholder="blurred"
+        />
+      </section>
+    </main>
   </Layout>
 );
 export default About;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,27 @@
 import * as React from "react";
 import { PageProps, Link } from "gatsby";
 import Layout from "../components/Layout";
-import Images from "../examples/images";
-
+import { StaticImage } from "gatsby-plugin-image";
+StaticImage;
 const Index = ({}: PageProps<{}>) => (
   <Layout>
-    <div>
-      <h1>home page</h1>
-      <Images></Images>
-    </div>
+    <main className="page">
+      <header className="hero">
+        <StaticImage
+          src="../assets/images/main.jpeg"
+          alt="eggs"
+          className="hero-img"
+          placeholder="tracedSVG"
+          layout="fullWidth"
+        ></StaticImage>
+        <div className="hero-container">
+          <div className="hero-text">
+            <h1>simple recipes</h1>
+            <h4>no fluff, just recipes</h4>
+          </div>
+        </div>
+      </header>
+    </main>
   </Layout>
 );
 export default Index;
