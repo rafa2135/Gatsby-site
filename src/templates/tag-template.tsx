4f50b3be684660d06b36f26bb55430ca46c8dbda
Rafael Villalobos <rafaelvillalobos_20@hotmail.com>
2023-03-04 01:46:52 -0400

    added tag-template content

--- src/templates/tag-template.tsx
+++ src/templates/tag-template.tsx
@@ -1,14 +1,45 @@
 import React from "react";
-import { PageProps } from "gatsby";
+import { PageProps, graphql } from "gatsby";
+import RecipesList from "../components/RecipesList";
+import Layout from "../components/Layout";
+import Recipes from "../interfaces/Recipes";
 
-const TagTemplate = (props: PageProps) => {
-  console.log(props);
-
+const TagTemplate = ({
+  data,
+  pageContext,
+}: PageProps<{
+  allContentfulRecipe: { nodes: Recipes[] };
+}>) => {
+  const recipes = data.allContentfulRecipe.nodes;
+  const tag = (pageContext as { tag: string }).tag;
   return (
-    <div>
-      <h2>tag template</h2>
-    </div>
+    <Layout>
+      <main className="page">
+        <h2>{tag}</h2>
+        <div className="tag-recipes">
+          <RecipesList recipes={recipes}></RecipesList>
+        </div>
+      </main>
+    </Layout>
   );
 };
+export const query = graphql`
+  query GetRecipeByTag($tag: String) {
+    allContentfulRecipe(
+      sort: { title: ASC }
+      filter: { content: { tags: { eq: $tag } } }
+    ) {
+      nodes {
+        title
+        id
+        cookTime
+        prepTime
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+        }
+      }
+    }
+  }
+`;
 
 export default TagTemplate;
